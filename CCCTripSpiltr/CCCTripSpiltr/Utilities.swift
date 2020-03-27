30c806973d0ef8e546111ccdfc069c922d0eafdc
JFerrer95 <jonathanferrer95@gmail.com>
2020-03-27 19:13:39 -0400

    Changed button shaped

--- CCCTripSpiltr/CCCTripSpiltr/Utilities.swift
+++ CCCTripSpiltr/CCCTripSpiltr/Utilities.swift
@@ -31,8 +31,8 @@ class Utilities {
     static func styleFilledButton(_ button:UIButton) {
         
         // Filled rounded corner style
-        button.backgroundColor = UIColor.init(red: 48/255, green: 173/255, blue: 99/255, alpha: 1)
-        button.layer.cornerRadius = 25.0
+        button.backgroundColor = UIColor.systemGreen
+        button.layer.cornerRadius = 18
         button.tintColor = UIColor.white
     }
     
@@ -41,7 +41,7 @@ class Utilities {
         // Hollow rounded corner style
         button.layer.borderWidth = 2
         button.layer.borderColor = UIColor.black.cgColor
-        button.layer.cornerRadius = 25.0
+        button.layer.cornerRadius = 18
         button.tintColor = UIColor.black
     }
     
