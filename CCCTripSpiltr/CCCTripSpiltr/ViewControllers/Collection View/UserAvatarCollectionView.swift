552aeed45724d9989feafa30887711ddc3aba7cc
JFerrer95 <jonathanferrer95@gmail.com>
2020-04-01 20:12:36 -0400

    trip detail, loading trip

---
+++ CCCTripSpiltr/CCCTripSpiltr/ViewControllers/Collection View/UserAvatarCollectionView.swift
@@ -0,0 +1,21 @@
+//
+//  UserAvatarCollectionView.swift
+//  CCCTripSpiltr
+//
+//  Created by jonathan ferrer on 4/1/20.
+//  Copyright © 2020 Ryan Murphy. All rights reserved.
+//
+
+import UIKit
+
+class UserAvatarCollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
