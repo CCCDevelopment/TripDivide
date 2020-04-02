452eacce0262dde5ba9e82b084985a0c5345262c
MurphDirt879 <boxert879@hotmail.com>
2020-04-02 18:32:59 -0400

    Number formatter for cost text field

--- CCCTripSpiltr/CCCTripSpiltr/ViewControllers/AddExpenseViewController.swift
+++ CCCTripSpiltr/CCCTripSpiltr/ViewControllers/AddExpenseViewController.swift
@@ -29,7 +29,18 @@ class AddExpenseViewController: UIViewController {
         configure()
         nameTextField.delegate = self
         costTextField.delegate = self
-     }
+    
+    
+    costTextField.addTarget(self, action: #selector(myTextFieldDidChange), for: .editingChanged)
+           }
+
+           @objc func myTextFieldDidChange(_ textField: UITextField) {
+
+               if let amountString = textField.text?.currencyInputFormatting() {
+                   textField.text = amountString
+               }
+           }
+
     
     override func viewWillAppear(_ animated: Bool) {
         print("Paid by \(expense.paidBy.count) people")
@@ -83,7 +94,42 @@ class AddExpenseViewController: UIViewController {
 extension AddExpenseViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         updateExpense()
+        
         textField.resignFirstResponder()
+        if textField == costTextField {
+            textField.text = textField.text?.currencyInputFormatting()
+        }
         return true
     }
+}
+
+extension String {
+
+    // formatting text for currency textField
+    func currencyInputFormatting() -> String {
+
+        var number: NSNumber!
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currencyAccounting
+        formatter.currencySymbol = "$"
+        formatter.maximumFractionDigits = 2
+        formatter.minimumFractionDigits = 2
+
+        var amountWithPrefix = self
+
+        // remove from String: "$", ".", ","
+        let regex = try! NSRegularExpression(pattern: "[^0-9]", options: .caseInsensitive)
+        amountWithPrefix = regex.stringByReplacingMatches(in: amountWithPrefix, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, self.count), withTemplate: "")
+
+        let double = (amountWithPrefix as NSString).doubleValue
+
+        number = NSNumber(value: (double / 100))
+        guard number != 0 as NSNumber else {
+            return ""
+        }
+
+        return formatter.string(from: number)!
+    }
+    
+    
 }
